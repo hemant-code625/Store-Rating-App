2010816740cce79c82ff02c00901e5442f752f64
Hemant Singh Parmar <hemantsinghparmar@mgmtech.org>
2025-12-09 11:09:24 +0530

    chore: remove unused getUserRatingController import from ratings.routes

--- backend/src/routes/ratings.routes.js
+++ backend/src/routes/ratings.routes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {
   submitRatingController,
-  getUserRatingController,
   getAllUserRatingsController,
   getAllStoreAveragesController,
   getStoreRatingsController,
